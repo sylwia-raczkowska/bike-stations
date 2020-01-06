d94ecdb1b94765c70702d3715fa91d157c7a30e9
sucuncu <sucuncu@consdata.pl>
2020-01-06 22:12:57 +0100

    Initial commit

--- src/main/java/com/raczkowska/citynav/station/StationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.raczkowska.citynav.station;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface StationRepository extends JpaRepository<Long, Station> {
-}
