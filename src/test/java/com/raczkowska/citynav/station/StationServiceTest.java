58fc22bd4fcca5897eb53a6198c650f22124cb47
sucuncu <sucuncu@consdata.pl>
2020-01-06 20:13:01 +0100

    handle exceptions

---
+++ src/test/java/com/raczkowska/citynav/station/StationServiceTest.java
@@ -0,0 +1,10 @@
+package com.raczkowska.citynav.station;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class StationServiceTest {
+
+}
